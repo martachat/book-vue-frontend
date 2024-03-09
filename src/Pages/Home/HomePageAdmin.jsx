128ce61a6d762ac9359c168438b4a9b7ba903661
Mohammed Ashour <mashour8@gmail.com>
2024-03-09 11:26:46 +0100

    done

--- src/Pages/Home/HomePageAdmin.jsx
+++ src/Pages/Home/HomePageAdmin.jsx
@@ -68,15 +68,15 @@ function HomePageAdmin({ islogedin }) {
 
   return (
     <div>
-      <input
-        type="text"
-        className="m-6 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg w-4/5 focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-        placeholder="Search"
-        value={searchQuery}
-        onChange={handleSearch}
-      />
       {islogedin ? (
         <div className="home_page">
+          <input
+            type="text"
+            className="m-6 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg w-4/5 focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="Search"
+            value={searchQuery}
+            onChange={handleSearch}
+          />
           {filteredBooks.map((book) => (
             <div key={book.id} className="container_home">
               <div className="container_home">
